0dc36f68aa0e7834d01642a028127f5f06f86e58
Andrei Laptsinski <gedenami@list.ru>
2019-05-18 22:20:04 +0300

    Add sctructure

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,9 +1,8 @@
 // ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <iostream>
 
-
-#include <iostream> 
 #include <windows.h> 
 #include <tlhelp32.h> 
 #include <tchar.h> 
@@ -11,9 +10,16 @@
 #include <string.h> 
 #include <vector> 
 
-
-
 using namespace std;
+
+
+struct Item {
+//	int info;
+	DWORD PID; //PID process
+	DWORD PPID; //Parent PID process
+	std::string name; //Name of process
+	Item* next;
+};
 
 
 
@@ -39,43 +45,117 @@ VOID PrintModuleList(HANDLE CONST hStdOut, DWORD CONST dwProcessId) {
 	CloseHandle(hSnapshot);
 }
 
-VOID PrintProcessList(HANDLE CONST hStdOut) {
+struct Item * PrintProcessList(HANDLE CONST hStdOut, Item *first) {
+	Item* p;
 	PROCESSENTRY32 peProcessEntry;
 	TCHAR szBuff[1024];
 	DWORD dwTemp;
 	HANDLE CONST hSnapshot = CreateToolhelp32Snapshot(
 		TH32CS_SNAPPROCESS, 0);
 	if (INVALID_HANDLE_VALUE == hSnapshot) {
-		return;
+		return first;
 	}
-
+	int i = 0;
 	peProcessEntry.dwSize = sizeof(PROCESSENTRY32);
 	Process32First(hSnapshot, &peProcessEntry);
 	do {
 		wsprintf(szBuff, L"=== %08X %s ===\r\n",
 			peProcessEntry.th32ProcessID, peProcessEntry.szExeFile);
-		WriteConsole(hStdOut, szBuff, lstrlen(szBuff), &dwTemp, NULL);
+		//WriteConsole(hStdOut, szBuff, lstrlen(szBuff), &dwTemp, NULL);
+
+		//wstring ws(peProcessEntry.szExeFile);
+		//string processName(ws.begin(), ws.end());
+
+
+		if (i == 0)
+		{
+			first->PID = peProcessEntry.th32ProcessID;
+			first->PPID = peProcessEntry.th32ParentProcessID;
+		
+			//TODO Fix
+			//first->name = processName;
+			first->next = NULL;
+			i = 1;
+
+		}
+		else 
+		{
+			p = new Item;
+			p->PID = peProcessEntry.th32ProcessID;
+			p->PPID = peProcessEntry.th32ParentProcessID;
+			p->next = first;
+			first = p;
+		}
 		//PrintModuleList(hStdOut, peProcessEntry.th32ProcessID);
 	} while (Process32Next(hSnapshot, &peProcessEntry));
 
 	CloseHandle(hSnapshot);
+	return (first);
+
 }
 
-struct process
+struct procesElement
 {
-	DWORD PID; // поле данных
-	char name[] = "";
-	struct list* ptr; // указатель на следующий элемент
+	DWORD PID; //PID process
+	DWORD PPID; //Parent PID process
+	std::string name; //Name of process
+	struct procesElement* tail; // указатель на следующий элемент
+	procesElement() {}
+	procesElement(DWORD a) { PID = a;}
+	procesElement(DWORD a, DWORD b) { PID = a; PPID = b; };
+	procesElement(DWORD p, DWORD pp, procesElement* t) { PID = p; PPID = pp, tail = t; };
+
 };
 
 
+struct procesElement * init(DWORD pid) // а- значение первого узла
+{
+	struct procesElement* procesElement;
+	// выделение памяти под корень списка
+	procesElement = (struct procesElement*)malloc(sizeof(struct procesElement));
+	procesElement->PID = pid;
+	procesElement->tail = NULL; // это последний узел списка
+	return(procesElement);
+}
 
+void listprint(Item* lst, DWORD curentpid, int count)
+{
+	struct Item* p;
+	p = lst;
+	
+	do 
+	{
+		if (p->PPID == curentpid)
+		{
+			for (int i = 0; i <= count; i++)
+			{
+				printf(" ");
+			}
+			printf("%d %d \n", p->PID, p->PPID); // вывод значения элемента p
+			listprint(lst, p->PID, count + 1);
+		}
+		p = p->next; // переход к следующему узлу
+	} while (p != NULL);
+}
 
 int main(void)
 {
+	//Item* first = 0; //Указатель на начало списка
+	Item* first = (struct Item*)malloc(sizeof(struct Item));
+	Item* p;
+	int i;
+	std::string name;
 
 	HANDLE CONST hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	PrintProcessList(hStdOut);
+	first = PrintProcessList(hStdOut, first);
+
+	listprint(first, 0, 0);
+
+
+	ExitProcess(0);
+
+	//GetProcessList();
+	return 0;
 
 	DWORD PID = 0x00051D28;
 	DWORD dwTemp;
@@ -98,21 +178,3 @@ int main(void)
 	//GetProcessList();
 	return 0;
 }
-
-
-
-/*int main()
-{
-    std::cout << "Hello World!\n";
-}
-*/
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
