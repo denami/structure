f6fc361d1b47df9a9be8857d0ab05e31464e1a62
Andrei Laptsinski <gedenami@list.ru>
2019-05-26 18:48:05 +0300

    Add menu

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -2,71 +2,120 @@
 #include <windows.h> 
 #include <tlhelp32.h> 
 #include <tchar.h> 
-#include <stdio.h> 
+#include <stdio.h>
 #include <string.h> 
 
 using namespace std;
 
-#define MAX_LINES = 20;
+#define MAX_LINES 20
 
 struct Item {
-	DWORD PID; //PID process
-	DWORD PPID; //Parent PID process
-	//std::wstring name;
-	std::string name;
-	int isPrinted = 0;
+	DWORD PID;          //PID process
+	DWORD PPID;         //Parent PID process
+	string name;        //Process exe file
+	int isPrinted = 0;  //Flag for marck process ass displaed
 
-	Item* next;
+	Item* next;         //handle to nex Item
 };
+
+
+int killProcess(DWORD p)
+{
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, p);
+	if (TerminateProcess(hProcess, 0))
+	{
+		printf("Prosecc  %d killed sucessful.\n", p);
+		return 0;
+	}
+	else
+	{
+		printf("Prosecc %d didn't killed.\n", p);
+		return 1;
+	}
+	return 1;
+}
+
+void displayKillMeny()
+{
+	printf("====================================\n");
+	printf("Type PID number for kill: \n");
+	DWORD p;
+
+	cin >> p;
+
+	killProcess(p);
+
+	printf("\n");
+}
 
 void displayMeny()
 {
+	printf("=====================================\n");
+	printf("| For kill process:       \t  1 |\n");
+	printf("| For print next page type:\t  2 |\n");
+	printf("| For quit type:          \t  3 |\n");
+	printf("=====================================\n");
+	char c;
+	int choise;
 
-
-
+	cin >> choise;
+	switch (choise)
+	{
+	case 1:
+	{
+		displayKillMeny();
+		break;
+	}
+	case 2:
+	{
+		return;
+		break;
+	}
+	case 3:
+	{
+		ExitProcess(0);
+		break;
+	}
+	default:
+		return;
+		break;
+	}
 }
 
-
-struct Item * PrintProcessList(HANDLE CONST hStdOut, Item *process) {
+struct Item * PrintProcessList(Item *process) {
 	Item* p;
-	PROCESSENTRY32 peProcessEntry; //Структура содержащая список процессов
+	PROCESSENTRY32 peProcessEntry; //Structure stored prosess list
 	TCHAR szBuff[1024];
 	DWORD dwTemp;
-	HANDLE CONST hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0); //Cоздает снапшот запущенных процессов
+	HANDLE CONST hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0); //Take snapshot for runned processes
 	if (INVALID_HANDLE_VALUE == hSnapshot) {
 		return process;
 	}
 	int i = 0;
 	peProcessEntry.dwSize = sizeof(PROCESSENTRY32);  //Set size of PROCESSENTRY32 structure
-	Process32First(hSnapshot, &peProcessEntry); //Receieve handle to the snapshot. Hendle to first element
+	Process32First(hSnapshot, &peProcessEntry);      //Receieve handle to the snapshot. Hendle to first element
 	do {
-		wsprintf(szBuff, L"=== %08X %s ===\r\n", peProcessEntry.th32ProcessID);
-			//, peProcessEntry.szExeFile);
-		//WriteConsole(hStdOut, szBuff, lstrlen(szBuff), &dwTemp, NULL);
-
-		//wsprintf("=== %d %d ===\r\n", peProcessEntry.th32ProcessID, peProcessEntry.szExeFile);
-
-		//_tprintf(TEXT("\n Process ID = 0x%08X"), peProcessEntry.th32ProcessID);
-
+		DWORD PID = peProcessEntry.th32ProcessID;
+		DWORD PPID = peProcessEntry.th32ParentProcessID;
 		wstring ws(peProcessEntry.szExeFile);
-		//std::string s(peProcessEntry.szExeFile);  //Variable s to store process exe name
 		string str(ws.begin(), ws.end());
-		if (i == 0)
+		
+		if (i == 0)  //Check for first iteration
 		{
-			process->PID = peProcessEntry.th32ProcessID;
-			process->PPID = peProcessEntry.th32ParentProcessID;
+			process->PID = PID;
+			process->PPID = PPID;
 			process->name = str;
 			process->next = NULL;
 			i = 1;
 		}
 		else 
 		{
-			p = new Item;
-			p->PID = peProcessEntry.th32ProcessID;
-			p->PPID = peProcessEntry.th32ParentProcessID;
+			p = new Item; //Create new Item
+			p->PID = PID;
+			p->PPID = PPID;
 			p->name = str;
-			p->next = process;
-			process = p;
+			p->next = process; //Set handle for current Item
+			process = p;       //Set hendle to new Item
 		}
 	}
 	while (Process32Next(hSnapshot, &peProcessEntry)); //Move handle to next element
@@ -76,11 +125,11 @@ struct Item * PrintProcessList(HANDLE CONST hStdOut, Item *process) {
 
 }
 
+//Method for check by PID for is this process in list
 BOOL processInList(Item* lst, DWORD pid)
 {
 	struct Item* p;  //handle for fist element of list
 	p = lst;
-
 	do
 	{
 		if (pid == p->PID)
@@ -94,8 +143,7 @@ BOOL processInList(Item* lst, DWORD pid)
 BOOL listprint(Item* lst, DWORD curentpid, int count, int line)
 {
 	struct Item* p;  //handle for fist element of list
-	p = lst;
-	//printf("curent pid %d cpace %d \n", curentpid, count);
+	p = lst;         //Counter for printed lines
 	do 
 	{
 		if (p->isPrinted == 0 &&  p->PPID == curentpid)
@@ -104,21 +152,25 @@ BOOL listprint(Item* lst, DWORD curentpid, int count, int line)
 			{
 				printf(" ");
 			}
-			//printf("%d %d %s\n", p->PID, p->PPID, p->name);
 			printf("%d  %s\n", p->PID,  p->name);
 			line++;
 			p->isPrinted = 1;
-			listprint(lst, p->PID, count + 1, line);
+			listprint(lst, p->PID, count + 1, 0); //Print  #MAX_LINES for each
 		}
 		if (p->isPrinted == 0 && !processInList(lst, p->PPID))
 		{
-			//printf("%d %d %s\n", p->PID, p->PPID, p->name);
 			printf("%d %s\n", p->PID, p->name);
 			line++;
 			p->isPrinted = 1;
-			listprint(lst, p->PID, 0, line);
+			listprint(lst, p->PID, 0, 0);
 			
 		}
+		if (line == MAX_LINES)
+		{
+			displayMeny();
+			line = 0;
+		}
+
 		p = p->next; //Move handle to next element
 	} while (p != NULL);
 	return 0;
@@ -126,43 +178,11 @@ BOOL listprint(Item* lst, DWORD curentpid, int count, int line)
 
 int main(void)
 {
-	//Item* first = 0; //Указатель на начало списка
-	Item* first = (struct Item*)malloc(sizeof(struct Item));
-	std::string name;
-
-	HANDLE CONST hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	first = PrintProcessList(hStdOut, first);
-
-
-	//_tprintf(TEXT("\n WARNING: %s failed with error %d (%s)"), L"aa", 11, L"02");
+	Item* first = new Item; //Create first Item (get memory for structure);
+	first = PrintProcessList(first);
 
 	listprint(first, 0, 0, 0);
+	displayMeny();
 
-
-
-	ExitProcess(0);
-
-	//GetProcessList();
-	return 0;
-
-	DWORD PID = 0x00051D28;
-	DWORD dwTemp;
-	TCHAR szBuff[1024];
-
-	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, PID);
-	if (TerminateProcess(hProcess, 0))
-	{
-		wsprintf(szBuff, L"=== %08X ---- Process was kiled ------ ===\r\n", PID);
-		WriteConsole(hStdOut, szBuff, lstrlen(szBuff), &dwTemp, NULL);
-	}
-	else
-	{
-		wsprintf(szBuff, L"=== %08X -----Can't kill process ----- ===\r\n", PID);
-		WriteConsole(hStdOut, szBuff, lstrlen(szBuff), &dwTemp, NULL);
-	}
-
-	ExitProcess(0);
-
-	//GetProcessList();
 	return 0;
 }
